a97b494e848fb33b245f336c34545810e1d68f9d
Balerto <Balerto@users.noreply.github.com>
2022-03-14 14:41:31 +0100

    Página web 2º Biología
    Página web 2º Biología

---
+++ JS/menu.js
@@ -0,0 +1,19 @@
+(function(){
+    const openbutton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu= document.querySelector('.nav__close');
+
+    openbutton.addEventListener('click',() => {
+        menu.classList.add('nav__link--show');
+       
+    });
+
+    closeMenu.addEventListener('click',()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+
+})();
+    
+
+
